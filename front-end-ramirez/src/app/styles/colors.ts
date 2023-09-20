fe395b525f7e11e6d642314010f4b88574cfc2cf
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-09-19 23:40:11 -0300

    feat: add colors variables

---
+++ front-end-ramirez/src/app/styles/colors.ts
@@ -0,0 +1,16 @@
+export const pallete = {
+  blackOne: "#212121",
+  blackTwo: "#434343",
+  blackThree: "#272727",
+  blackFour: "#545454",
+  blackFive: "#121214",
+  blackSix: "#313131",
+  grayOne: "#c4c4c4",
+  grayTwo: "#979797",
+  grayThree: "#bbbbbb",
+  whiteOne: "#ffffff",
+  turquoiseOne: "#40e0d0",
+  green: "#3CB371",
+  red: "#FF6347",
+  yellow: "#EFB700"
+}
