5831e98b1ed340e45bccb99eabc58bb4eea8f184
Wilhelm Steins <wilhelmsteins@gmail.com>
2023-10-01 16:09:21 -0300

    fix(header): resolve css import error styled component

--- front-end-ramirez/src/app/pages/home/style.ts
+++ front-end-ramirez/src/app/pages/home/style.ts
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { pallete } from "../../styles/colors";
 
 interface HeaderProps {
