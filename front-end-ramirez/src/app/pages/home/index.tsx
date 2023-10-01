5120be947c7bbc6f6d7b1b2c8f654803c22967ed
Wilhelm Steins <wilhelmsteins@gmail.com>
2023-10-01 16:11:34 -0300

    feat(header): initial index header

--- front-end-ramirez/src/app/pages/home/index.tsx
+++ front-end-ramirez/src/app/pages/home/index.tsx
@@ -1,13 +1,44 @@
 "use client"
-import { Container} from "./style";
+import { Container, Header, HeaderContent, LogoImage, MenuContainer, Button  } from "./style";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function HomePage() {
+    const [menuIsActive, setMenuIsActive] = useState(false);
+
     return (
         <Container 
             initial={{width: 0}} 
             animate={{width: "100%"}} 
             exit={{ x: 100, transition: { duration: 0.6 } }}>
-            oi
+            <Header toggleMenu={menuIsActive}>
+                <HeaderContent>
+                    <LogoImage />
+                    <MenuContainer>
+                        <ul>
+                            <li>
+                                <Button>Sobre</Button>
+                            </li>
+                            <li>
+                                <Link href="/login">
+                                    <Button>Log-in</Button>
+                                </Link>
+                            </li>
+                            <li>
+                                /
+                            </li>
+                            <li>
+                                <Link href="/signup">
+                                    <Button background={true}>
+                                        Sign-up
+                                    </Button>
+                                </Link>
+                            </li>
+                        </ul>
+                    </MenuContainer>
+                </HeaderContent>
+            </Header>
         </Container>
     )
 }
