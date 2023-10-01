0df5419f855eea5f861229320133c7145e231461
Wilhelm Steins <wilhelmsteins@gmail.com>
2023-10-01 16:02:11 -0300

    feat(header): create header styled component

--- front-end-ramirez/src/app/pages/home/style.ts
+++ front-end-ramirez/src/app/pages/home/style.ts
@@ -2,6 +2,10 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import { pallete } from "../../styles/colors";
 
+interface HeaderProps {
+    toggleMenu: boolean;
+}
+
 export const Container = styled(motion.div)`
     display: flex;
     flex-direction: column;
@@ -11,4 +15,43 @@ export const Container = styled(motion.div)`
     width: 100%;
     background-color: ${pallete.blackOne};
     color: ${pallete.whiteOne};
+`
+
+export const Header = styled.header<HeaderProps>`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 3.75rem;
+    transition: all ease 0.2s;
+
+    &::after {
+        content: '';
+        display: block;
+        bottom: 0;
+        height: 1px;
+        width: 93%;
+        margin: 0 auto;
+        background-color: ${pallete.whiteOne};
+    }
+
+    @media screen and (max-width: 771px) {
+        position: fixed;
+        top: 0;
+        left: ${props => props.toggleMenu ? "0%" : "-100%"};
+        height: 100vh;
+        background-color: ${pallete.blackOne};
+        z-index: 999999;
+
+        &::after {
+            content: '';
+            display: none;
+        }
+
+        div {
+            flex-direction: column;
+        }
+    }
 `
