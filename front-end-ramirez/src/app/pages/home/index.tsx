99517567325fb182558822afc88a53b5172755dd
isaniovitor <isaniovitot@gmail.com>
2023-10-01 17:28:56 -0300

    feature: logo in home page

--- front-end-ramirez/src/app/pages/home/index.tsx
+++ front-end-ramirez/src/app/pages/home/index.tsx
@@ -1,12 +1,39 @@
 "use client"
-import { Container, Header, HeaderContent, LogoImage, MenuContainer, Button, MenuIcon } from "./style";
-import { useState } from "react";
+import { Container, Header, HeaderContent, LogoImage, MenuContainer, Button, MenuIcon, Main,  Services} from "./style";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import logoHome from "../../assets/logo-home.svg";
 import { List, X } from "phosphor-react";
+import { useInView } from "react-intersection-observer";
+import { useAnimation, motion } from "framer-motion";
+import { Slider } from "../../components/Slider";
+
+let easing = [0.6, -0.05, 0.01, 0.99];
 
 export default function HomePage() {
     const [menuIsActive, setMenuIsActive] = useState(false);
+    const { ref, inView } = useInView();
+    const animation = useAnimation();
+
+    useEffect(() => {
+        if (inView) {
+            animation.start({
+                y: 0,
+                opacity: 1,
+                transition: {
+                    duration: 0.8,
+                    ease: easing,
+                }
+            })
+        } else {
+            animation.start({
+                y: 30,
+                opacity: 0,
+                transition: { duration: 0.6, ease: easing }
+            });
+        }
+    }, [animation, inView])
 
     return (
         <Container 
@@ -48,6 +75,12 @@ export default function HomePage() {
                     </MenuContainer>
                 </HeaderContent>
             </Header>
+            <Main>
+                <motion.div>
+                    <Image alt={"logo"} src={logoHome}/>
+                </motion.div>
+                <Slider/>
+            </Main>
         </Container>
     )
 }
