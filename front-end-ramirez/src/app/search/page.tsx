39406272397b5fb5b7082f68ffc07b0868651bf7
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-10-04 21:22:16 -0300

    feat: add simple inputs for filters

--- front-end-ramirez/src/app/search/page.tsx
+++ front-end-ramirez/src/app/search/page.tsx
@@ -1,7 +1,27 @@
 "use client"
 
 import { PhotographerCard } from "../components/PhotographerCard"
-import { Container, InputName, PhotographersList, SearchButton, SearchInputContainer, SearchPhotographerContainer } from "./style"
+import {
+  AditionalInputs,
+  Container,
+  Divider,
+  GenericInput,
+  InputName,
+  LocationAccordion,
+  PhotographersList,
+  PriceRange,
+  PriceRangeContainer,
+  SearchButton,
+  SearchInputContainer,
+  SearchPhotographerContainer,
+} from "./style"
+
+const orderOptions = [
+  { name: "Nenhum", field: null },
+  { name: "Visitas", field: "views" },
+  { name: "Curtidas", field: "likes" },
+  { name: "Preço", field: "price" },
+]
 
 export default function Search() {
   return (
@@ -11,21 +31,55 @@ export default function Search() {
         <SearchInputContainer>
           <div>
             <InputName
-              type="text" 
+              type="text"
               placeholder="Digite aqui o nome de um fotógrafo"
               required
             />
             <SearchButton>Pesquisar</SearchButton>
           </div>
+          <AditionalInputs>
+            <PriceRangeContainer>
+              <PriceRange>Faixa de preço:</PriceRange>
+              <label htmlFor="minPrice"></label>
+              <GenericInput
+                type="number"
+                id="minPrice"
+                name="minPrice"
+                placeholder="Mínimo"
+              />
+              <Divider />
+              <label htmlFor="maxPrice"></label>
+              <GenericInput
+                type="number"
+                id="maxPrice"
+                name="maxPrice"
+                placeholder="Máximo"
+              />
+            </PriceRangeContainer>
+            <LocationAccordion isActive={true}>
+              <label htmlFor="location">
+                <div></div>
+                Local
+              </label>
+              <div>
+                <GenericInput
+                  type="text"
+                  id="location"
+                  name="location"
+                  placeholder="Quixadá"
+                />
+              </div>
+            </LocationAccordion>
+          </AditionalInputs>
         </SearchInputContainer>
         <PhotographersList>
-          <PhotographerCard/>
-          <PhotographerCard/>
-          <PhotographerCard/>
-          <PhotographerCard/>
-          <PhotographerCard/>
-          <PhotographerCard/>
-          <PhotographerCard/>
+          <PhotographerCard />
+          <PhotographerCard />
+          <PhotographerCard />
+          <PhotographerCard />
+          <PhotographerCard />
+          <PhotographerCard />
+          <PhotographerCard />
         </PhotographersList>
       </SearchPhotographerContainer>
     </Container>
