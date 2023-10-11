e360c8a8457110e1a2fe13e69a005f95515ca2af
isaniovitor <isaniovitot@gmail.com>
2023-10-11 20:47:29 -0300

    fix: animation fix

--- front-end-ramirez/src/app/pages/home/index.tsx
+++ front-end-ramirez/src/app/pages/home/index.tsx
@@ -42,8 +42,8 @@ export default function HomePage() {
 
     return (
         <Container 
-            initial={{width: 0}} 
-            animate={{width: "100%"}} 
+            initial={{width: 0, opacity: 0}} 
+            animate={{width: "100%", opacity: 1}} 
             exit={{ x: 100, transition: { duration: 0.6 } }}
         >
             <MenuIcon onClick={() => setMenuIsActive(!menuIsActive)}>
