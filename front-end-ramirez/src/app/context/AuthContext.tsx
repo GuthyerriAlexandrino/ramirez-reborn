7bde15a2593cdc6e51df8139354f3af4b00d61ae
isaniovitor <isaniovitot@gmail.com>
2023-10-20 22:56:54 -0300

    feature: auth context

--- front-end-ramirez/src/app/context/AuthContext.tsx
+++ front-end-ramirez/src/app/context/AuthContext.tsx
@@ -50,6 +50,85 @@ export function AuthProvider({children}: AuthProviderProps) {
       } 
 
   }, [])
+
+  // futura integração
+  async function getProfileImage() {
+      const cookies = parseCookies();
+      const token = cookies['ramirez-user']
+      const id = cookies['ramirez-user-id']
+
+      const data = await fetch('', {}).then(res => res.json());
+
+      if (data.profile_img === "") {
+          return;
+      }
+
+      const foresRef = ref(storage, data.profile_img);
+      await getDownloadURL(foresRef)
+      .then(url => {
+          setUserProfileImage(url)
+      })
+      .catch(error => console.log(error));
+  }
+
+  function verifyTokenExpiration() {
+      const cookies = parseCookies();
+      const token = cookies['ramirez-user']
+
+      if (!token) {
+          return false;
+      } 
+      return true;
+  }
+
+  function removeToken() {
+      destroyCookie(null, "ramirez-user")
+      destroyCookie(null, "ramirez-user-id")
+  }
+
+  async function handleLogin({email, password}: User) {
+      const user = {
+          user: {
+              email: email, 
+              password: password
+          }
+      }
+
+      // futura integração
+      const res = await fetch("", {})
+      .then(response => response.json())
+      .catch(error => console.log(error))
+
+      if (res.error) {
+          notifyError("Falha no login! Verifique os campos preenchidos.")
+      } else {
+
+          setCookie(undefined, "ramirez-user", res.token, {
+              expires: new Date(res.exp)
+          })
+
+          setCookie(undefined, "ramirez-user-id", res.user.$oid, {
+              expires: new Date(res.exp)
+          })
+  
+          notifySuccess("Login feito com sucesso!")
+          Router.push("/search")
+      }
+  }
+
+  return (
+      <AuthContext.Provider
+          value={{
+              userProfileImage,
+              getProfileImage,
+              removeToken,
+              verifyTokenExpiration,
+              handleLogin
+          }}
+      >
+          {children}
+      </AuthContext.Provider>
+  )
 }
 
 export const useAuthLogin = () => {
