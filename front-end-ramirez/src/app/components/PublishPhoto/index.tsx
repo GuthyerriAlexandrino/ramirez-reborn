c6dff2a68ae77c7fcf8462fb3b0afbfc99e42977
isaniovitor <isaniovitot@gmail.com>
2023-10-21 15:03:40 -0300

    feature: finished publish photo component

--- front-end-ramirez/src/app/components/PublishPhoto/index.tsx
+++ front-end-ramirez/src/app/components/PublishPhoto/index.tsx
@@ -70,6 +70,14 @@ export function PublishPhoto({handlePopUp}: PublishPhotoProps) {
       console.log(res)
   }
 
+  function changePhotoPrice(value: number) {
+      if (value < 0) {
+          setPhotoPrice((photoPrice! - 1) < 0 ? 0 : photoPrice! - 1);
+          return;
+      }
+      setPhotoPrice(photoPrice! + 1)
+  }
+
   function verifyIfSizesImageIsValid() {
       const {value, sizes} = formatBytes(photoImageContent?.size!);
 
@@ -77,6 +85,14 @@ export function PublishPhoto({handlePopUp}: PublishPhotoProps) {
           return false;
       }
       return true;
+  }
+
+  function renderSizeOfImage() {
+      const {value, sizes} = formatBytes(photoImageContent?.size!);
+
+      return (
+          <strong>{value!} <sub>{sizes}</sub></strong>
+      )
   }
 
   return (
@@ -92,7 +108,84 @@ export function PublishPhoto({handlePopUp}: PublishPhotoProps) {
                   method="post" 
                   onSubmit={addNewPost}
               >
-                 
+                  <InputContainer>
+                      <label htmlFor="title">Título</label>
+                      <InputValue 
+                          type="text" 
+                          id="title" 
+                          name="title" 
+                          placeholder="Digite aqui um título para sua foto"
+                          required
+                          onChange={(event) => setPhotoTitle(event.target.value)}
+                      />
+                  </InputContainer>
+                  <InputContainer>
+                      <label htmlFor="price">Preço</label>
+                      <Icon 
+                          title="Valor mínimo"
+                          data-title="minus"
+                          valuePosition={40}  
+                          onClick={() => changePhotoPrice(-1)}
+                      >
+                          <Minus 
+                              size={24} 
+                              color={pallete.red} 
+                              weight="bold" 
+                          />
+                      </Icon>
+                      <Icon 
+                          title="Valor máximo"
+                          data-title="plus"
+                          valuePosition={10}
+                          onClick={() => changePhotoPrice(1)}
+                      >
+                          <Plus 
+                              size={24} 
+                              color={pallete.green} 
+                              weight="bold" 
+                          />
+                      </Icon>
+                      <InputValue
+                          id="price" 
+                          name="price" 
+                          type="number"
+                          value={photoPrice === null ? "" : photoPrice} 
+                          onChange={(event) => setPhotoPrice(Number(event.target.value))}
+                          placeholder="Digite aqui o preço da voto. Ex.: 100"
+                          min={0}
+                      />
+                  </InputContainer>
+                  <InputContainer>
+                      <label>Foto {renderSizeOfImage()} </label>
+                      <InputFileLabel>
+                          <span>{photoImageContent ? `> ${photoImageContent.name}` : "Insira aqui uma foto"}</span>
+                          {photoImageContent ? (
+                              <PreviewImage>
+                                  <Image 
+                                      src={URL.createObjectURL(photoImageContent!)} 
+                                      alt="foto para publicação"
+                                      layout="fixed"
+                                      width={100}
+                                      height={100}
+                                      objectFit="cover"
+                                  />
+                              </PreviewImage>
+                          ) : (
+                              <IconContainer>
+                                  <FilePlus color={pallete.grayTwo} weight="fill" size={40}/>
+                              </IconContainer>
+                          )}
+                          <InputFile
+                              type="file"
+                              id="file"
+                              name="file"
+                              placeholder="Insira aqui uma foto"
+                              required
+                              accept=".jpg,.jpeg,.png"
+                              onChange={(e) => e.target.files && setPhotoImageContent(e.target.files[0]!)}
+                          />
+                      </InputFileLabel>
+                  </InputContainer>
                   <ButtonSubmit type="submit">Postar foto</ButtonSubmit>
               </FormArea>
           </FormAreaContainer>            
